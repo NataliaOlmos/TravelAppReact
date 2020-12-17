2fd0825c1c0b2903e95519f13ab62034e5d0092b
Natalia <nataliav_4428@hotmail.com>
2020-12-16 23:21:57 -0600

    landing page and navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Pages/Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "App">
+      <Router>
+      <Navbar />
+
+      <Switch>
+          <Route exact path="/" component={Home}>
+          </Route>
+          
+        </Switch>
+    </Router>
     </div>
   );
 }

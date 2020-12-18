ed61f3ce341a1384dd2cbca4885334b847355c8a
Natalia <nataliav_4428@hotmail.com>
2020-12-17 20:14:40 -0600

    sreen styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Pages/Home'
+import Services from './components/Pages/Services'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
   } from "react-router-dom";
+import Products from './components/Pages/Products';
+import SignUp from './components/Pages/SignUp';
 
 function App() {
   return (
@@ -17,7 +20,12 @@ function App() {
       <Switch>
           <Route exact path="/" component={Home}>
           </Route>
-          
+          <Route exact path="/services" component={Services}>
+          </Route>
+          <Route exact path="/products" component={Products}>
+          </Route>
+          <Route exact path="/sign-up" component={SignUp}>
+          </Route>
         </Switch>
     </Router>
     </div>
---
+++ src/components/Pages/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import '../../App.css'
+
+function SignUp(){
+    return(
+        <h1 className="sign-up">Sign Up</h1>
+    )
+}
+
+export default SignUp
